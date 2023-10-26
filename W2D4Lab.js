1a3cb32c46403e043ba0d1e57f7cb6d2d6182716
djenki13 <danieledmundjenkins@gmail.com>
2023-10-26 12:33:00 -0400

    completed lab

---
+++ W2D4Lab.js
@@ -0,0 +1,32 @@
+// Write a while loop for the given output:
+//  10, 30, 50, 70, 90
+//  20, 40, 60, 80, 100
+
+// First set
+// let i=10
+
+// while (i <= 100) {
+//     console.log(i);
+//     i += 20;
+// }
+// console.log(i)
+
+// Second set
+
+// let i=20
+
+// while (i <= 100) {
+//     console.log(i);
+//     i += 20;
+// }
+// console.log(i)
+
+// Write a while loop that prints all numbers from 500 - 800 only if they are divisible by 4.
+
+// let i=500
+
+// while (i<=800) {
+//     if (i % 4 ===0) {console.log(i);
+//     } i++
+// }
+// console.log(i)
